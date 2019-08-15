62955fc3e4e2aca73e1b7144e2891e546b56914b
Dhan V Sagar <dhanvsagar@gmail.com>
2019-08-15 16:35:25 +0530

    Updated queue

--- queue/main.go
+++ queue/main.go
@@ -10,19 +10,30 @@ func (q *Queue) Engueue(data int) {
 	q.slice = append(q.slice, data)
 }
 
-func (q *Queue) Dequeue() int {
-	data := q.slice[0]
-	q.slice = q.slice[1:len(q.slice)]
-	return data
+func (q *Queue) Dequeue() (int, error) {
+	if len(q.slice) < 0 {
+		return -1, fmt.Errorf("No items in the queue")
+	} else {
+		data := q.slice[0]
+		q.slice = q.slice[1:len(q.slice)]
+		return data, nil
+	}
+
 }
 
 func main() {
-
 	new_q := Queue{}
 	new_q.Engueue(10)
 	new_q.Engueue(1)
 	new_q.Engueue(12)
 	fmt.Println(new_q)
 
-	fmt.Println(new_q.Dequeue())
+	data, err := new_q.Dequeue()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(data)
+	}
+
+	fmt.Println(new_q)
 }
